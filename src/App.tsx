e85145e49c1f68a60f4407f485fee608eb90105c
Dmitriy Trifonov <trifonov.d@divan.ru>
2024-08-29 09:01:49 +0300

    feature: Add AddNewTicker modal boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState, MouseEvent } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 import getCandles from "api/getCandles";
 import getTickers from "storage/getTickers";
 import TickerCard from "components/TickerCard/TickerCard";
+import AddNewTicker from "modals/AddNewTicker/AddNewTicker";
 
 const today = new Date();
 
@@ -22,6 +25,7 @@ export type Candle = {
 
 function App() {
   const [data, setData] = useState<Candle[][]>([]);
+  const [addTickerModalOpen, setAddTickerModalOpen] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -37,9 +41,33 @@ function App() {
     fetchData();
   }, []);
 
+  const showAddTickerModal = useCallback(() => {
+    setAddTickerModalOpen(true);
+  }, []);
+
+  const handleOk = () => {
+    setAddTickerModalOpen(false);
+  };
+
+  const handleCancel = (e: MouseEvent<HTMLButtonElement>) => {
+    setAddTickerModalOpen(false);
+  };
+
   return (
     <div>
       <h1>myShares</h1>
+      <Button
+        type="primary"
+        icon={<PlusOutlined />}
+        onClick={showAddTickerModal}
+      >
+        Add Ticker
+      </Button>
+      <AddNewTicker
+        open={addTickerModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      />
       <h2>
         Period: <span>{lastMonth.toISOString().split("T")[0]}</span> -&nbsp;
         <span>{today.toISOString().split("T")[0]}</span>
