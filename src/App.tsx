1db12ac4d795ced9c3dba3065541f59677cbacfc
Dmitriy Trifonov <trifonov.d@divan.ru>
2024-10-11 08:42:22 +0300

    feature: Add AddTickerButton appearing transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConfigProvider } from "antd";
+import { useInView } from "react-intersection-observer";
+import cn from "classnames";
 
 import Initializer from "components/Initializer/Initializer";
 import Header from "components/Header/Header";
 import TickerCardsList from "components/TickerCardsList/TickerCardsList";
 import AddTickerButton from "components/AddTickerButton/AddTickerButton";
 import AddNewTickerModal from "modals/AddNewTickerModal/AddNewTickerModal";
+import styles from "./App.module.css";
 
 const queryClient = new QueryClient();
 
 function App() {
   const [addTickerModalOpen, setAddTickerModalOpen] = useState(false);
+  const [addTickerButtonHidden, setAddTickerButtonHidden] = useState(true);
   const handleOk = () => setAddTickerModalOpen(false);
   const handleCancel = () => setAddTickerModalOpen(false);
+  const { ref } = useInView({
+    onChange: (inView) => {
+      if (inView) setAddTickerButtonHidden(true);
+      else setAddTickerButtonHidden(false);
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
       <ConfigProvider theme={{ token: { colorPrimary: "#ec9706" } }}>
         <Initializer />
-        <Header setAddTickerModalOpen={setAddTickerModalOpen} />
+        <Header ref={ref} setAddTickerModalOpen={setAddTickerModalOpen} />
         <TickerCardsList />
-        <AddTickerButton setAddTickerModalOpen={setAddTickerModalOpen} />
+        <AddTickerButton
+          className={cn(styles.addTickerButton, {
+            [styles.hidden]: addTickerButtonHidden,
+          })}
+          setAddTickerModalOpen={setAddTickerModalOpen}
+        />
         <AddNewTickerModal
           open={addTickerModalOpen}
           onOk={handleOk}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import { PlusOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 
@@ -5,34 +6,36 @@ import formatDate from "utils/formatDate";
 import { lastMonth, today } from "data";
 import styles from "./Header.module.css";
 
-import type { FC, Dispatch, SetStateAction } from "react";
+import type { Dispatch, SetStateAction } from "react";
 
 interface HeaderProps {
   setAddTickerModalOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const Header: FC<HeaderProps> = ({ setAddTickerModalOpen }) => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.topline}>
-        <h1>myShares</h1>
-      </div>
-      <div className={styles.subheader}>
-        <Button
-          type="primary"
-          icon={<PlusOutlined />}
-          onClick={() => setAddTickerModalOpen(true)}
-        >
-          Add Ticker
-        </Button>
+const Header = forwardRef<HTMLDivElement, HeaderProps>(
+  ({ setAddTickerModalOpen }, ref) => {
+    return (
+      <header ref={ref} className={styles.header}>
+        <div className={styles.topline}>
+          <h1>myShares</h1>
+        </div>
+        <div className={styles.subheader}>
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+            onClick={() => setAddTickerModalOpen(true)}
+          >
+            Add Ticker
+          </Button>
 
-        <h2>
-          Period: <span>{formatDate(lastMonth)}</span> -&nbsp;
-          <span>{formatDate(today)}</span>
-        </h2>
-      </div>
-    </header>
-  );
-};
+          <h2>
+            Period: <span>{formatDate(lastMonth)}</span> -&nbsp;
+            <span>{formatDate(today)}</span>
+          </h2>
+        </div>
+      </header>
+    );
+  }
+);
 
 export default Header;
